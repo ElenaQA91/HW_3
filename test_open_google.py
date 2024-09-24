180953a6b767cf4ac36c86ab1ef4f90dd322ace5
Elena <elenakolesnikova141@gmail.com>
2024-09-24 20:17:45 +0500

    1 автотест прошел успешно

---
+++ test_open_google.py
@@ -0,0 +1,7 @@
+from selene import browser, be, have
+
+
+def test_open_brawser():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
