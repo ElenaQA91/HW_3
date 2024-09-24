92f4948bc34de2ee9f6f8f0d8c73463717990758
Elena <elenakolesnikova141@gmail.com>
2024-09-24 20:23:10 +0500

    тест прошел успешно

--- test_open_google.py
+++ test_open_google.py
@@ -5,3 +5,9 @@ def test_open_brawser():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+def test_unsuccessful_brawser():
+    text = '868689698^%&%&%^&*'
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type(text).press_enter()
+    browser.element('.card-section').should(have.text(f"По запросу {text} ничего не найдено"))
